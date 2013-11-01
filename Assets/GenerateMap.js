646755cd707e30cf6c13ecfaef44da62175fbf8a
Brian Glen <br.glen@yahoo.com>
2013-10-31 21:28:30 -0400

    Started on map generation.

---
+++ Assets/GenerateMap.js
@@ -0,0 +1,27 @@
+#pragma strict
+
+var size;
+var totalSkyboxes;
+
+function generateMap(size){
+	//draw random number, 1 to totalSyboxes
+	//select case and apply skybox to main camera
+	
+	//generate planets based on size variable
+	
+	//generate random integer numbers for x , y
+	//check if they are too close to planets
+	//check if they collide with another asteroid via asteroid array
+	//if true, regenerate numbers
+	//else, draw asteroids, add to asteroid array
+	
+	//space stations and other stuff
+
+function Start () {
+	//set size to player's input
+	generateMap(size);
+}
+
+function Update () {
+
+}
